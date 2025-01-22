18452fe117b3e4b2c402f6d50ee646c1b356185a
Leonardo Tolosa <tolosa@gmail.com>
2025-01-22 14:04:28 -0300

    Add MUI components and basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-  return <>Add content here</>;
-}
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#242424",
+    },
+  },
+});
+
+const App = () => (
+  <ThemeProvider theme={theme} noSsr>
+    <CssBaseline />
+    <p>TODO: add content</p>
+  </ThemeProvider>
+);
 
 export default App;
