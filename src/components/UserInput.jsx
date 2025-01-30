c903fb49939f7fbeaf0428977f1d0815d74492dd
Leonardo Tolosa <tolosa@gmail.com>
2025-01-30 00:17:19 -0300

    Clean up markup

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,34 +1,30 @@
 import { TextField, IconButton, Icon } from "@mui/material";
 import PropTypes from "prop-types";
 
-const UserInput = ({ inputValue, isLoading, onInputChange, onSendMessage }) => {
-  return (
-    <>
-      <TextField
-        label="Ask Luffy"
-        fullWidth
-        value={inputValue}
-        disabled={isLoading}
-        onChange={(e) => onInputChange(e.target.value)}
-        onKeyDown={(e) => e.key == "Enter" && onSendMessage(inputValue)}
-        slotProps={{
-          input: {
-            sx: { borderRadius: 3 },
-            endAdornment: (
-              <IconButton
-                edge="end"
-                onClick={() => onSendMessage(inputValue)}
-                disabled={isLoading || !inputValue.trim()}
-              >
-                <Icon>send</Icon>
-              </IconButton>
-            ),
-          },
-        }}
-      />
-    </>
-  );
-};
+const UserInput = ({ inputValue, isLoading, onInputChange, onSendMessage }) => (
+  <TextField
+    label="Ask Luffy"
+    fullWidth
+    value={inputValue}
+    disabled={isLoading}
+    onChange={(e) => onInputChange(e.target.value)}
+    onKeyDown={(e) => e.key == "Enter" && onSendMessage(inputValue)}
+    slotProps={{
+      input: {
+        sx: { borderRadius: 3 },
+        endAdornment: (
+          <IconButton
+            edge="end"
+            onClick={() => onSendMessage(inputValue)}
+            disabled={isLoading || !inputValue.trim()}
+          >
+            <Icon>send</Icon>
+          </IconButton>
+        ),
+      },
+    }}
+  />
+);
 
 UserInput.propTypes = {
   inputValue: PropTypes.string.isRequired,
